a0daebcdb249d58b9638622ca5f5072187cc9f27
Bulat <gink@MacBook-Air-Bulat.local>
2023-09-05 15:34:58 +0300

    add vocalize

--- src/models/translationUnit.ts
+++ src/models/translationUnit.ts
@@ -93,6 +93,19 @@ const TranslationUnitModel = {
     
     preloadImage(unit: TranslationUnit): void {
         preloadImages(unit.imageSrc);
+    },
+
+    vocalize(unit: TranslationUnit): void {
+        const msg = new SpeechSynthesisUtterance();
+        const voices = window.speechSynthesis.getVoices();
+
+        msg.voice = voices[0];
+        msg.volume = 1; // From 0 to 1
+        msg.rate = 1; // From 0.1 to 10
+        msg.pitch = 1; // From 0 to 2
+        msg.text = unit.translation;
+        msg.lang = unit.transLang;
+        speechSynthesis.speak(msg);
     }
 };
 
--- src/widgets/game/middle/components/unit/index.tsx
+++ src/widgets/game/middle/components/unit/index.tsx
@@ -1,13 +1,20 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import styles from '../../styles.module.scss';
 import {useSelector} from 'react-redux';
 import {selectCurrentUnit, selectCurrentUnitNumber} from '../../../../../store/selectors/game';
+import TranslationUnitModel from '../../../../../models/translationUnit';
 
 type UnitProps = { reverse?: boolean };
 
 const Unit: FC<UnitProps> = ({ reverse }) => {
     const currentUnit = useSelector(selectCurrentUnit);
     const currentUnitNumber = useSelector(selectCurrentUnitNumber);
+
+    useEffect(() => {
+        if (currentUnit) {
+            TranslationUnitModel.vocalize(currentUnit);
+        }
+    }, [currentUnit]);
 
     return (
         <div className={styles.unitToStudy}>
